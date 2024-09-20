98c4361e67ca5d8cea561e20d72b345ad9d7d66c
rinishat <142586403+rinishat@users.noreply.github.com>
2024-09-20 22:04:15 +0530

    updated contact

--- sample1/src/pages/Contact.jsx
+++ sample1/src/pages/Contact.jsx
@@ -9,7 +9,9 @@ const Contact = () => {
           <h1 className="text-3xl font-bold">I'd love to hear from you. </h1>
         </div>
         <div className="flex-1 flex justify-center items-center">
-          <h1 className="text-3xl font-bold"></h1>
+          <h1 className="text-xl">rinishajesline@gmail.com</h1>
+          <p className="text-xl">7540017432</p>
+          <h1 className="text-xl"></h1>
         </div>
       </div>
     </>
